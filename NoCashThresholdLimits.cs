414dfe84f88ef53fccb71ed159b8c82a7e918027
FermentedPeach <mikepack20082@hotmail.com>
2025-09-26 02:24:13 -0500

    Cash popup and display now properly show above 9,999,999 cash xD

--- NoCashThresholdLimits.cs
+++ NoCashThresholdLimits.cs
@@ -3,11 +3,15 @@ using BTD_Mod_Helper.Extensions;
 using HarmonyLib;
 using Il2CppAssets.Scripts.Models.Rounds;
 using Il2CppAssets.Scripts.Unity.UI_New.InGame;
+using Il2CppAssets.Scripts.Unity.UI_New.InGame.Stats;
 using Il2CppAssets.Scripts.Unity.UI_New.Popups;
 using Il2CppInterop.Runtime.InteropTypes.Arrays;
 using Il2CppTMPro;
 using MelonLoader;
 using NoCashThresholdLimits;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 [assembly: MelonInfo(typeof(NoCashThresholdLimits.NoCashThresholdLimits), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
@@ -26,6 +30,7 @@ public class NoCashThresholdLimits : BloonsTD6Mod
         thresholds[0] = threshold;
 
         game.GetGameModel().incomeSet = new IncomeSetModel("_NewIncomeSet", thresholds, 1.0f);
+
     }
 
     public override void OnApplicationStart()
@@ -33,8 +38,13 @@ public class NoCashThresholdLimits : BloonsTD6Mod
         ModHelper.Msg<NoCashThresholdLimits>("NoCashThresholdLimits loaded!");
     }
 
+    static public void Log(ConsoleColor color, object message)
+    {
+        if (message != null)
+            MelonLogger.Msg(color, message);
+    }
 
-    //Fix the cash and health popups to give up to 9 999 999 999 999 money/health
+    //Fix the cash popup to give above 9 999 999 cash :)
     [HarmonyPatch(typeof(Popup), nameof(Popup.ShowPopup))]
     internal static class Popup_ShowPopup
     {
@@ -42,10 +52,33 @@ public class NoCashThresholdLimits : BloonsTD6Mod
         private static void Prefix(Popup __instance)
         {
 
-            if (__instance.gameObject.name.Contains("SetValuePopup"))
+            if (__instance.gameObject.name.Contains("SetValuePopup") && __instance.title.m_text.Contains("Set Cash"))
             {
-                __instance.GetComponentInChildren<TMP_InputField>().characterLimit = 13;
+                var inputField = __instance.GetComponentInChildren<TMP_InputField>();
+                var button = __instance.GetComponentInChildren<Button>();
+
+                inputField.characterLimit = 19; //assume signed long (64 bit)
+                button.AddOnClick(() =>
+                {
+                    double outVal = 0.0;
+                    double.TryParse(inputField.m_Text.Replace(",", "").Replace("$", ""), out outVal);
+                    Log(System.ConsoleColor.Red, outVal);
+                    InGame.instance.bridge.SetCash(outVal);
+                });
             }
         }
     }
+
+    //Fix the cash display to display above 9 999 999 cash
+    [HarmonyPatch(typeof(CashDisplay), nameof(CashDisplay.OnCashChanged))]
+    internal static class CashDisplay_OnCashChanged
+    {
+        [HarmonyPostfix]
+        private static void Postfix(CashDisplay __instance)
+        {
+            __instance.text.SetText("$" + ((ulong)InGame.instance.bridge.GetCash()).ToString("N0"));
+            Log(System.ConsoleColor.Cyan, InGame.instance.bridge.GetCash());
+        }
+    }
+
 }
